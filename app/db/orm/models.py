4bda6c6f645b232d371b0a2b9333ebc79bb46ef4
Alexey Naumov <digitaal.vier@gmail.com>
2024-06-22 23:32:10 +0200

    remove annotations

--- app/db/orm/models.py
+++ app/db/orm/models.py
@@ -1,19 +1,12 @@
-from datetime import datetime
-
 from sqlalchemy import Boolean, DateTime, Integer, String, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, mapped_column
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
-    # id: Mapped[UUID] = mapped_column(
-    #     UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
-    # )
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    created_on: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), default=func.now()
-    )
-    updated_on: Mapped[datetime] = mapped_column(
+    id = mapped_column(Integer, primary_key=True)
+    created_on = mapped_column(DateTime(timezone=True), default=func.now())
+    updated_on = mapped_column(
         DateTime(timezone=True), default=func.now(), onupdate=func.now()
     )
 
@@ -21,11 +14,11 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = "users"
 
-    login: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(String, nullable=False)
-    refresh_token: Mapped[str] = mapped_column(String, nullable=True)
-    access_token: Mapped[str] = mapped_column(String, nullable=True)
-    is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
+    login = mapped_column(String, unique=True, nullable=False)
+    password = mapped_column(String, nullable=False)
+    refresh_token = mapped_column(String, nullable=True)
+    access_token = mapped_column(String, nullable=True)
+    is_admin = mapped_column(Boolean, default=False)
 
     def __repr__(self) -> str:
         return f"User(id={self.id}, login={self.login}, is_admin={self.is_admin})"
