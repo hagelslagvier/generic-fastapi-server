249a719fd38bc886d6ff875317044f8bf4c681ba
Alexey Naumov <digitaal.vier@gmail.com>
2024-06-22 23:41:12 +0200

    remove leftovers

--- tests/test_endpoints/conftest.py
+++ tests/test_endpoints/conftest.py
@@ -4,15 +4,6 @@ from sqlalchemy import Engine
 from sqlalchemy.orm import Session
 from starlette.testclient import TestClient
 
-# @pytest.fixture
-# def config():
-#     with patch("app.assembly.dotenv_values") as mock:
-#         config = {"DB_URI": "sqlite:///"}
-#         mock.return_value = config
-#
-#         yield config
-#
-#
 from app.db.orm.crud.common import UserCRUD
 from app.db.orm.models import Base
 from tests.assembly import test_root_injector
@@ -28,12 +19,6 @@ def db():
 @pytest.fixture
 def content(db):
     session = test_root_injector.get(Session)
-
-    # login: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    # password: Mapped[str] = mapped_column(String, nullable=False)
-    # refresh_token: Mapped[str] = mapped_column(String, nullable=True)
-    # access_token: Mapped[str] = mapped_column(String, nullable=True)
-    # is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
 
     user_crud = UserCRUD()
     for index in range(5):
