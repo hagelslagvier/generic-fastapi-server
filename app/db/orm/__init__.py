77269c104bc763c14f3ac02eb33597ea02466096
Alexey Naumov <digitaal.vier@gmail.com>
2024-06-30 23:32:16 +0200

    remove leftovers

--- app/db/orm/__init__.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-
-from sqlalchemy import create_engine
-
-db_path = pathlib.Path(__file__).parents[1] / "db.sqlite3"
-db_url = f"sqlite:///{db_path}"
-
-engine = create_engine(db_url)
