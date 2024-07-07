ac014d0313a2b726b5ec9056a7b3f9b404808e64
Alexey Naumov <digitaal.vier@gmail.com>
2024-07-07 17:18:31 +0200

    fix bug

--- app/run.py
+++ app/run.py
@@ -2,9 +2,11 @@ import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
+from app.assembly import root_injector
+
 load_dotenv()
 
-app = FastAPI()
+app = root_injector.get(FastAPI)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True)
