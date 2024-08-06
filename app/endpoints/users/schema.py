77d321430dcd70db6fa141d97a1480cf6991b866
Alexey Naumov <digitaal.vier@gmail.com>
2024-08-07 01:55:11 +0200

    fix user schema

--- app/endpoints/users/schema.py
+++ app/endpoints/users/schema.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 
 
 class UserSchemaInput(BaseModel):
-    login: str
-    password: str
+    login: str = Field(..., max_length=64)
+    password: str = Field(..., max_length=64)
+    email: EmailStr
+    is_email_confirmed: bool = False
     refresh_token: Optional[str] = None
     access_token: Optional[str] = None
     is_admin: bool = False
