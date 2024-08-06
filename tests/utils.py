f09e5d2f86fcd31ef5f4a30f1e4ededf8d690d5a
Alexey Naumov <digitaal.vier@gmail.com>
2024-08-07 01:51:54 +0200

    silence warning

--- tests/utils.py
+++ tests/utils.py
@@ -5,6 +5,6 @@ def merge_metadata(*chunks: MetaData) -> MetaData:
     metadata = MetaData()
     for chunk in chunks:
         for table_name, table in chunk.tables.items():
-            metadata._add_table(table_name, table.schema, table)
+            metadata._add_table(table_name, table.schema, table)  # noqa
 
     return metadata
