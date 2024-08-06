3d9846236677811684a1f5174df984ea32557946
Alexey Naumov <digitaal.vier@gmail.com>
2024-08-07 01:56:02 +0200

    refactor endpoints tests

--- tests/test_endpoints/conftest.py
+++ tests/test_endpoints/conftest.py
@@ -4,33 +4,29 @@ from sqlalchemy import Engine
 from starlette.testclient import TestClient
 
 from app.db.orm.crud.common import UserCRUD
-from app.db.orm.crud.interfaces import SessionFactoryInterface
-from app.db.orm.models import Base
+from app.db.orm.crud.generic import session_factory
 from tests.assembly import test_root_injector
 from tests.types import SideEffect
 
 
 @pytest.fixture
-def db() -> None:
-    engine = test_root_injector.get(Engine)
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-
-
-@pytest.fixture
-def content(db: SideEffect) -> None:
-    payload = [
-        {"login": f"foo_{index}", "password": f"bar_{index}"} for index in range(5)
-    ]
-    session_factory = test_root_injector.get(SessionFactoryInterface)  # type: ignore[type-abstract]
-    with session_factory.make() as session:
+def content(engine: Engine) -> None:
+    with session_factory(bind=engine) as session:
         user_crud = UserCRUD(session=session)
-        user_crud.create_many(payload=payload)
+        user_crud.create_many(
+            payload=[
+                {
+                    "login": f"user-{index}",
+                    "password": f"password-{index}",
+                    "email": f"user-{index}@abc.com",
+                }
+                for index in range(5)
+            ]
+        )
 
 
 @pytest.fixture
-def test_client(content: SideEffect) -> TestClient:
+def test_client(content: SideEffect, engine: Engine) -> TestClient:
     app = test_root_injector.get(FastAPI)
     client = TestClient(app)
-
     return client
--- tests/test_endpoints/test_users.py
+++ tests/test_endpoints/test_users.py
@@ -1,30 +1,55 @@
 from starlette.testclient import TestClient
 
-from app.endpoints.users.schema import UserSchemaInput
+from app.endpoints.users.schema import UserSchemaInput, UserSchemaOutput
 
 
 def test_if_can_get_many(test_client: TestClient) -> None:
     response = test_client.get("/users")
     assert response.status_code == 200
+    assert {UserSchemaOutput(**item).id for item in response.json()} == {1, 2, 3, 4, 5}
 
 
 def test_if_can_get_one(test_client: TestClient) -> None:
     response = test_client.get("/users/1")
     assert response.status_code == 200
+    assert UserSchemaOutput(**response.json()).dict(
+        exclude={"created_on", "updated_on"}
+    ) == {
+        "id": 1,
+        "login": "user-0",
+        "password": "password-0",
+        "email": "user-0@abc.com",
+        "is_email_confirmed": False,
+        "refresh_token": None,
+        "access_token": None,
+        "is_admin": False,
+    }
 
 
 def test_if_can_post(test_client: TestClient) -> None:
-    user_schema = UserSchemaInput(login="foo", password="bar")
-    response = test_client.post("/users", json=user_schema.dict())
+    input_schema = UserSchemaInput(login="foo", password="bar", email="foo@bar.baz")
+    response = test_client.post("/users", json=input_schema.dict())
     assert response.status_code == 200
+    assert (
+        UserSchemaOutput(**response.json()).dict(
+            exclude={"id", "created_on", "updated_on"}
+        )
+        == input_schema.dict()
+    )
 
 
 def test_if_can_put(test_client: TestClient) -> None:
-    user_schema = UserSchemaInput(login="foo", password="bar")
-    response = test_client.put("/users/1", json=user_schema.dict())
+    input_schema = UserSchemaInput(
+        login="foo-new", password="bar-new", email="foo-new@bar.baz"
+    )
+    response = test_client.put("/users/1", json=input_schema.dict())
     assert response.status_code == 200
+    assert UserSchemaOutput(**response.json()).dict(
+        exclude={"created_on", "updated_on"}
+    ) == {"id": 1, **input_schema.dict()}
 
 
 def test_if_can_delete(test_client: TestClient) -> None:
     response = test_client.delete("/users/1")
     assert response.status_code == 200
+    assert UserSchemaOutput(**response.json()).id == 1
