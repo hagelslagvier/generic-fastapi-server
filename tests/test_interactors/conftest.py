a74ad2deabcd68a7781efc9cf6162b6c12cb6e21
Alexey Naumov <digitaal.vier@gmail.com>
2025-02-22 22:24:28 +0100

    Fix tests

--- tests/test_interactors/conftest.py
+++ tests/test_interactors/conftest.py
@@ -1,12 +1,21 @@
+import socket
+from collections.abc import Generator
+from pathlib import Path
+from tempfile import gettempdir
+
 import pytest
-from inzicht import session_factory
-from sqlalchemy import Engine
-
-from app.interactors.readiness.interactors import ReadinessStatusCRUD
 
 
-@pytest.fixture
-def content(engine: Engine) -> None:
-    with session_factory(bind=engine) as session:
-        payload = {"hostname": "foo", "ready": False}
-        ReadinessStatusCRUD(session=session).create(payload=payload)
+@pytest.fixture(scope="session")
+def lock_file_path() -> str:
+    file_name = socket.gethostname()
+    file_path = Path(gettempdir()) / file_name
+    file_path.unlink(missing_ok=True)
+    return str(file_path)
+
+
+@pytest.yield_fixture
+def lock_file(lock_file_path: str) -> Generator[None, None, None]:
+    open(lock_file_path, "w")
+    yield
+    Path(lock_file_path).unlink(missing_ok=True)
--- tests/test_interactors/test_readiness_probe.py
+++ tests/test_interactors/test_readiness_probe.py
@@ -1,23 +1,22 @@
-from sqlalchemy import Engine
-
 from app.interactors.readiness.interactors import ReadinessProbe
 from tests.aliases import SideEffect
 
 
-def test_if_can_check_readiness(engine: Engine, content: SideEffect) -> None:
-    probe = ReadinessProbe(bind=engine, hostname="foo")
-    assert probe.hostname == "foo"
-    assert probe.is_ready() is False
+def test_if_can_check_readiness(lock_file_path: str) -> None:
+    probe = ReadinessProbe()
+    assert not probe.is_ready()
 
 
-def test_if_can_set_readiness(engine: Engine, content: SideEffect) -> None:
-    probe = ReadinessProbe(bind=engine, hostname="foo")
+def test_if_can_set_readiness(lock_file: SideEffect) -> None:
+    probe_1 = ReadinessProbe()
+    assert probe_1.is_ready()
 
-    probe.set_ready(True)
-    assert probe.is_ready() is True
+    probe_1.set_ready(False)
+    assert not probe_1.is_ready()
 
-    probe.set_ready(False)
-    assert probe.is_ready() is False
+    probe_2 = ReadinessProbe()
+    assert not probe_2.is_ready()
 
-    probe.set_ready(True)
-    assert probe.is_ready() is True
+    probe_2.set_ready(True)
+    assert probe_2.is_ready()
+    assert probe_1.is_ready()
