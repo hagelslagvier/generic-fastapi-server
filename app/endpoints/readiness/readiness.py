53fc994e97d86d4d072630adee22fd9326575d79
Alexey Naumov <digitaal.vier@gmail.com>
2025-02-23 00:39:43 +0100

    Refactor token usage

--- app/endpoints/readiness/readiness.py
+++ app/endpoints/readiness/readiness.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter, Depends
 from injector import Injector
 
-from app.database.orm.models import User
-from app.dependencies.auth import get_user_from_token
 from app.dependencies.injector import make_injector
 from app.endpoints.readiness.schema import ReadinessStatus
 from app.interactors.readiness.interfaces import ReadinessProbeInterface
@@ -15,7 +13,6 @@ router = APIRouter(
 
 @router.get("/")
 def get(
-    client: User | None = Depends(get_user_from_token),
     injector: Injector = Depends(make_injector),
 ) -> ReadinessStatus:
     readiness_probe = injector.get(ReadinessProbeInterface)
