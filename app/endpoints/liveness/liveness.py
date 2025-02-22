a1a9e4f00274035debdf6e3f2c8a14eabcbaed4d
Alexey Naumov <digitaal.vier@gmail.com>
2025-02-23 00:39:43 +0100

    Refactor token usage

--- app/endpoints/liveness/liveness.py
+++ app/endpoints/liveness/liveness.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter, Depends
 from injector import Injector
 
-from app.database.orm.models import User
-from app.dependencies.auth import get_user_from_token
 from app.dependencies.injector import make_injector
 from app.endpoints.liveness.schema import LivenessStatus
 from app.interactors.liveness.interfaces import LivenessProbeInterface
@@ -15,7 +13,6 @@ router = APIRouter(
 
 @router.get("/")
 def get(
-    client: User | None = Depends(get_user_from_token),
     injector: Injector = Depends(make_injector),
 ) -> LivenessStatus:
     liveness_probe = injector.get(LivenessProbeInterface)
