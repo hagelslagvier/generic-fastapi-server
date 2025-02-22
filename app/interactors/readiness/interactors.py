ffa3c9f1eccf16ae1c8a91c6d6b4fe386df172bc
Alexey Naumov <digitaal.vier@gmail.com>
2025-02-22 22:22:14 +0100

    Add deps

--- app/interactors/readiness/interactors.py
+++ app/interactors/readiness/interactors.py
@@ -1,43 +1,20 @@
 import socket
+from pathlib import Path
+from tempfile import gettempdir
 
-from inzicht import GenericCRUD, session_factory
-from sqlalchemy import Engine
-
-from app.database.orm.models import ReadinessStatus
 from app.interactors.readiness.interfaces import ReadinessProbeInterface
 
 
-class ReadinessStatusCRUD(GenericCRUD[ReadinessStatus]):
-    pass
-
-
 class ReadinessProbe(ReadinessProbeInterface):
-    def __init__(self, bind: Engine, hostname: str = "") -> None:
-        self.engine = bind
-        self.hostname = hostname or socket.gethostname()
+    def __init__(self) -> None:
+        self.file_path = Path(gettempdir()) / socket.gethostname()
 
     def is_ready(self) -> bool:
-        with session_factory(bind=self.engine) as session:
-            items = ReadinessStatusCRUD(session=session).read_many(
-                where=ReadinessStatus.hostname == self.hostname
-            )
-            found = list(items)
-            if len(found) == 1:
-                [probe] = found
-                return probe.ready
-            else:
-                return False
+        return self.file_path.exists()
 
     def set_ready(self, ready: bool) -> None:
-        with session_factory(bind=self.engine) as session:
-            items = ReadinessStatusCRUD(session=session).read_many(
-                where=ReadinessStatus.hostname == self.hostname
-            )
-            found = list(items)
-            if len(found) == 0:
-                ReadinessStatusCRUD(session=session).create(
-                    payload={"ready": ready, "hostname": self.hostname}
-                )
-            else:  # since unique constraint
-                [probe] = found
-                probe.ready = ready
+        if ready:
+            path = str(self.file_path)
+            open(path, "w")
+        else:
+            Path.unlink(self.file_path, missing_ok=True)
